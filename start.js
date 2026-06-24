0ae55b1929ccbf781c535e21651053e1354e4230
Elijah agwuncha <eli963411@gmail.com>
2026-06-25 00:09:10 +0100

    fix pairing for railway

--- start.js
+++ start.js
@@ -1,8 +1,14 @@
-// start.js - Force pairing code for Railway
-console.log("🚀 Starting FAM Bug Bot with Pairing Code...");
+console.log("🚀 Starting FAM Bug Bot - Forcing Pairing Code Mode...");
 
 process.env.PAIRING_CODE = "true";
 process.env.AUTO_UPDATE = "false";
 process.env.PHONE_NUMBER = "2349124173525";
+
+// Bypass readline crash
+const originalQuestion = require('readline').Interface.prototype.question;
+require('readline').Interface.prototype.question = function(query, callback) {
+    console.log("🔄 [BYPASS] readline question skipped");
+    if (callback) callback("2349124173525");  // your number
+};
 
 require('./index.js');
